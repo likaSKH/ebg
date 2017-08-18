4f06c4135e3b46ec8e70a8a3bd377873c98b7216
likaSKH <lika.sikharulia@gmail.com>
2017-08-18 15:57:37 +0400

    git add files

---
+++ application/views/welcome_message.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+
+    <link rel="stylesheet" href="../public/bootstrap/css/bootstrap.min.css">
+
+</head>
+<body>
+
+
+
+</body>
+</html>
